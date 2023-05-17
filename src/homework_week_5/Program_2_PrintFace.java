29328398ac6a30420235299fe8b2e8051e2bf41b
Bhavnanand <Bjanand1411@gmail.com>
2023-05-17 18:11:08 +0100

    Homework Done

---
+++ src/homework_week_5/Program_2_PrintFace.java
@@ -0,0 +1,17 @@
+package homework_week_5;
+
+/**
+ * 2. Write a java progrsm to print a face.
+ */
+public class Program_2_PrintFace
+{
+    public static void main(String[] args)
+    {
+        System.out.println("+\"\"\"\"\"+");
+        System.out.println("[|  o o  |]");
+        System.out.println(" |   ^   |");
+        System.out.println(" |  '  ' |");
+        System.out.println(" |   _   |");
+        System.out.println(" +_______+");
+    }
+}
